eea277ec7a5b1f21313d2315393cb278e07d005b
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-08 14:16:33 +0200

    Add files via upload

---
+++ zadanie3/zadanie3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj wagę [kg]:");
+        double waga = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj wzrost [m]:");
+        double wzrost = Convert.ToDouble(Console.ReadLine());
+
+        double bmi = waga / (wzrost * wzrost);
+
+        Console.WriteLine("Twój współczynnik BMI wynosi: " + bmi);
+
+        Console.ReadKey();
+    }
+}
