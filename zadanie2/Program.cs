6ee439a3b220f7d250e0fafae4a9ad515493dc00
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-08 13:45:05 +0200

    Dodaj pliki projektów.

---
+++ zadanie2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {              
+        Console.WriteLine("Podaj pole koła:");
+
+        double pole = Convert.ToDouble(Console.ReadLine());
+        
+        double promien = Math.Sqrt(pole / Math.PI);
+        
+        double obwod = 2 * Math.PI * promien;
+        
+        Console.WriteLine("Obwód koła wynosi: " + obwod);
+
+        Console.ReadKey();
+    }
+}
