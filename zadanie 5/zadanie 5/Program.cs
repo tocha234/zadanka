4b129196ac52220ea532cdece7b04e1827bebf3e
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-08 14:19:24 +0200

    Add files via upload

---
+++ zadanie 5/zadanie 5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj pierwszą liczbę:");
+        int liczba1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podaj drugą liczbę:");
+        int liczba2 = Convert.ToInt32(Console.ReadLine());
+
+        int wiekszaLiczba = Math.Max(liczba1, liczba2);
+        Console.WriteLine("Większa liczba to: " + wiekszaLiczba);
+
+        Console.ReadKey();
+    }
+}
