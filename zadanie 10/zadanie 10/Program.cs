3c3db442863e431ed89800ae0495633b28a44ddf
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-08 14:22:44 +0200

    Add files via upload

---
+++ zadanie 10/zadanie 10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę naturalną:");
+        int liczba = Convert.ToInt32(Console.ReadLine());
+
+        int ostatniaCyfra = liczba % 10;
+
+        Console.WriteLine("Ostatnia cyfra liczby {0} to: {1}", liczba, ostatniaCyfra);
+
+        Console.ReadKey();
+    }
+}
