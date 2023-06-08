105f8c86ccd4a0393bf356e6dc598fc0c34750ec
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-08 14:19:55 +0200

    Add files via upload

---
+++ zadanie 6/zadanie 6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj długość pierwszego boku:");
+        double bok1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj długość drugiego boku:");
+        double bok2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj długość trzeciego boku:");
+        double bok3 = Convert.ToDouble(Console.ReadLine());
+
+        bool czyTrojkatRownoboczny = bok1 == bok2 && bok2 == bok3;
+
+        if (czyTrojkatRownoboczny)
+        {
+            Console.WriteLine("Podane boki tworzą trójkąt równoboczny.");
+        }
+        else
+        {
+            Console.WriteLine("Podane boki nie tworzą trójkąta równobocznego.");
+        }
+
+        Console.ReadKey();
+    }
+}
