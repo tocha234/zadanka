ab7a5ce3755cc7a5b0e1564a5e854ab785b0542e
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-08 14:18:06 +0200

    Add files via upload

---
+++ zadanie1/zadanie1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj pierwszą liczbę:");
+        double liczba1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj drugą liczbę:");
+        double liczba2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj trzecią liczbę:");
+        double liczba3 = Convert.ToDouble(Console.ReadLine());
+
+        double srednia = (liczba1 + liczba2 + liczba3) / 3;
+
+        Console.WriteLine("Średnia arytmetyczna podanych liczb wynosi: " + srednia);
+
+        Console.ReadKey();
+    }
+}
