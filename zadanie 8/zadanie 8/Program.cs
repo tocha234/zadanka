b1901ddef80225474767f7edc7491e71477f2cba
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-08 14:21:24 +0200

    Add files via upload

---
+++ zadanie 8/zadanie 8/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj pierwszą liczbę:");
+
+        double liczba1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj drugą liczbę:");
+
+        double liczba2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Wybierz działanie:");
+        Console.WriteLine("1. Dodawanie");
+        Console.WriteLine("2. Odejmowanie");
+        Console.WriteLine("3. Mnożenie");
+        Console.WriteLine("4. Dzielenie");
+
+        int numerDzialania = Convert.ToInt32(Console.ReadLine());
+
+        string wynik = "";
+
+        switch (numerDzialania)
+        {
+            case 1:
+                wynik = (liczba1 + liczba2).ToString();
+                break;
+            case 2:
+                wynik = (liczba1 - liczba2).ToString();
+                break;
+            case 3:
+                wynik = (liczba1 * liczba2).ToString();
+                break;
+            case 4:
+                if (liczba2 != 0)
+                {
+                    wynik = (liczba1 / liczba2).ToString();
+                }
+                else
+                {
+                    wynik = liczba1.ToString() + "/0";
+                }
+                break;
+            default:
+                Console.WriteLine("Nieprawidłowy numer działania.");
+                break;
+        }
+
+
+        Console.WriteLine("Wynik: " + wynik);
+
+
+
+        Console.ReadKey();
+    }
+}
