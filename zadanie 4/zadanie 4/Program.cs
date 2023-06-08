e936af156433857c9fd84906c09ccff705b8130f
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-08 14:18:48 +0200

    Add files via upload

---
+++ zadanie 4/zadanie 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę a:");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podaj liczbę b:");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podaj liczbę c:");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podaj liczbę d:");
+        int d = Convert.ToInt32(Console.ReadLine());
+
+        if (b == 0 || d == 0)
+        {
+            Console.WriteLine("Brak rozwiązania");
+        }
+        else
+        {
+            int x = a * d + b * c;
+            int y = b * d;
+            Console.WriteLine("Licznik (x): " + x);
+            Console.WriteLine("Mianownik (y): " + y);
+        }
+        Console.ReadKey();
+    }
+}
