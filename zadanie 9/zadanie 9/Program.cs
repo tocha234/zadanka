265dd50996a622991b6bbc2bad3dd898531c5cae
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-08 14:21:58 +0200

    Add files via upload

---
+++ zadanie 9/zadanie 9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj wartość a:");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podaj wartość b:");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podaj wartość c:");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        b = b + c;
+        c = b - c;
+        b = b - c;
+
+        Console.WriteLine("Po zamianie miejscami:");
+        Console.WriteLine("a: " + a);
+        Console.WriteLine("b: " + b);
+        Console.WriteLine("c: " + c);
+
+        Console.ReadKey();
+    }
+}
