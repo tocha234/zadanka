01abba70822dabbd70e725339c8e16d0ea7c2277
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-16 10:08:06 +0200

    Add files via upload

---
+++ algorytmy zadanie 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj podstawę potęgi:");
+        int podstawa = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podaj wykładnik potęgi:");
+        int wykladnik = Convert.ToInt32(Console.ReadLine());
+
+        int wynik = Potegowanie(podstawa, wykladnik);
+
+        Console.WriteLine("Wynik: " + wynik);
+    }
+
+    static int Potegowanie(int podstawa, int wykladnik)
+    {
+        int wynik = 1;
+
+        for (int i = 0; i < wykladnik; i++)
+        {
+            wynik *= podstawa;
+        }
+
+        return wynik;
+    }
+}
