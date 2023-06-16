5cef5d44c4291cdabc698e61d16de7b9ee0eb758
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-16 10:11:21 +0200

    Add files via upload

---
+++ algorytmy zadanie 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę n:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Pary liczb naturalnych o sumie " + n + ":");
+
+        WypiszPary(n);
+    }
+
+    static void WypiszPary(int n)
+    {
+        for (int i = 1; i <= n / 2; i++)
+        {
+            int j = n - i;
+            Console.WriteLine(i + " + " + j);
+        }
+    }
+}
