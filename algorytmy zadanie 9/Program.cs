b09021d916f0625e8cbbd889ed497b0527459658
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-16 10:12:16 +0200

    Add files via upload

---
+++ algorytmy zadanie 9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę:");
+        int liczba = Convert.ToInt32(Console.ReadLine());
+
+        int odwrocona = OdwrocLiczbe(liczba);
+
+        Console.WriteLine("Liczba po odwróceniu: " + odwrocona);
+    }
+
+    static int OdwrocLiczbe(int liczba)
+    {
+        int odwrocona = 0;
+
+        while (liczba > 0)
+        {
+            int cyfra = liczba % 10;
+            odwrocona = (odwrocona * 10) + cyfra;
+            liczba /= 10;
+        }
+
+        return odwrocona;
+    }
+}
