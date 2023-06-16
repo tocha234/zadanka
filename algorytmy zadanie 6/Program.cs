2078f6b17e0914f6234baf44be69b01ebafb60d0
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-16 10:10:51 +0200

    Add files via upload

---
+++ algorytmy zadanie 6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę x:");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        int n = SprawdzSilnie(x);
+
+        if (n != -1)
+        {
+            Console.WriteLine(x + " jest silnią liczby " + n);
+        }
+        else
+        {
+            Console.WriteLine(x + " nie jest silnią żadnej liczby");
+        }
+    }
+
+    static int SprawdzSilnie(int x)
+    {
+        int n = 0;
+        int silnia = 1;
+
+        while (silnia <= x)
+        {
+            if (silnia == x)
+            {
+                return n;
+            }
+
+            n++;
+            silnia *= n;
+        }
+
+        return -1;
+    }
+}
