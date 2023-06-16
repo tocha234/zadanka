085494b5b48e2c9d54a4850504556bb8f63cd0aa
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-16 10:09:43 +0200

    Add files via upload

---
+++ algorytmy zadanie 4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj kwotę początkową na koncie:");
+        decimal kwotaPoczatkowa = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Podaj oprocentowanie konta w skali roku:");
+        decimal oprocentowanie = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Podaj liczbę miesięcy oszczędzania:");
+        int liczbaMiesiecy = Convert.ToInt32(Console.ReadLine());
+
+        decimal kwotaKoncowa = ObliczKwoteKoncowa(kwotaPoczatkowa, oprocentowanie, liczbaMiesiecy);
+
+        Console.WriteLine("Kwota końcowa: " + kwotaKoncowa);
+    }
+
+    static decimal ObliczKwoteKoncowa(decimal kwotaPoczatkowa, decimal oprocentowanie, int liczbaMiesiecy)
+    {
+        decimal oprocentowanieMiesieczne = oprocentowanie / 12 / 100;
+        decimal kwotaKoncowa = kwotaPoczatkowa * (decimal)Math.Pow(1 + (double)oprocentowanieMiesieczne, liczbaMiesiecy);
+        kwotaKoncowa *= (decimal)Math.Pow(1 - 0.19, liczbaMiesiecy); // uwzględnienie podatku Belki (19%)
+
+        return kwotaKoncowa;
+    }
+}
