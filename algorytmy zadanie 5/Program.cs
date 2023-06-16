be27e00ea4ad0bacc79ffe5eed32c6d3c467aae1
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-16 10:10:11 +0200

    Add files via upload

---
+++ algorytmy zadanie 5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj wartość x:");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj liczbę wyrazów n:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        double wynik = ObliczWyrazenie(x, n);
+
+        Console.WriteLine("Wartość e^x: " + wynik);
+    }
+
+    static double ObliczWyrazenie(double x, int n)
+    {
+        double wynik = 0;
+        double potega = 1;
+        double silnia = 1;
+
+        for (int i = 0; i <= n; i++)
+        {
+            wynik += potega / silnia;
+            potega *= x;
+            silnia *= i + 1;
+        }
+
+        return wynik;
+    }
+}
