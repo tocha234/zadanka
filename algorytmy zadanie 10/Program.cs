41af0129da160d6b8d045ed5f9ccbbfa02f1f195
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-16 10:12:43 +0200

    Add files via upload

---
+++ algorytmy zadanie 10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę naturalną o co najmniej 3 cyfrach:");
+        int liczba = Convert.ToInt32(Console.ReadLine());
+
+        if (liczba < 100)
+        {
+            Console.WriteLine("Podana liczba ma mniej niż 3 cyfry.");
+            return;
+        }
+
+        Console.WriteLine("Poszczególne cyfry liczby:");
+
+        WypiszCyfry(liczba);
+    }
+
+    static void WypiszCyfry(int liczba)
+    {
+        string liczbaJakoTekst = liczba.ToString();
+
+        foreach (char cyfra in liczbaJakoTekst)
+        {
+            Console.WriteLine(cyfra);
+        }
+    }
+}
