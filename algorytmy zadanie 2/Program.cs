ca30082a88e054ed6342a2e5c6d233e9f35874ae
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-16 10:08:40 +0200

    Add files via upload

---
+++ algorytmy zadanie 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę wyrazów ciągu Fibonacciego do wygenerowania:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ciąg Fibonacciego:");
+
+        for (int i = 0; i < n; i++)
+        {
+            int wynik = Fibonacci(i);
+            Console.Write(wynik + " ");
+        }
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
