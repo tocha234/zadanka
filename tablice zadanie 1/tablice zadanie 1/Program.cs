e1dedc098aadeb0d9efc33beee6ba1542b28bcf0
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-23 16:01:37 +0200

    Add files via upload

---
+++ tablice zadanie 1/tablice zadanie 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] tablica = GenerujTablice(10); // generowanie tablicy o rozmiarze 10
+        WypiszTabliceOdTylu(tablica);
+    }
+
+    static int[] GenerujTablice(int rozmiar)
+    {
+        int[] tablica = new int[rozmiar];
+        Random random = new Random();
+
+        for (int i = 0; i < rozmiar; i++)
+        {
+            tablica[i] = random.Next(100); // generowanie losowej wartości od 0 do 99
+        }
+
+        return tablica;
+    }
+
+    static void WypiszTabliceOdTylu(int[] tablica)
+    {
+        for (int i = tablica.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine(tablica[i]);
+        }
+    }
+}
