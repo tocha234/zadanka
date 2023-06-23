c0f13d8bf8a00221a6980bee84ae22493bcaf50f
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-23 16:01:06 +0200

    Add files via upload

---
+++ iteracje zadanie 3/iteracje zadanie 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj wysokość trójkąta: ");
+        int height = int.Parse(Console.ReadLine());
+
+        int width = 2 * height - 1;
+
+        for (int row = 1; row <= height; row++)
+        {
+            int numSymbols = 2 * row - 1;
+            int numSpaces = (width - numSymbols) / 2;
+
+            for (int space = 0; space < numSpaces; space++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int symbol = 0; symbol < numSymbols; symbol++)
+            {
+                Console.Write("+");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
