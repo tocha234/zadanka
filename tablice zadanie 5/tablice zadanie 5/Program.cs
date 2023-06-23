53a93a8919653d88da48944dbe519fc1d1325cd5
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-23 16:04:10 +0200

    Add files via upload

---
+++ tablice zadanie 5/tablice zadanie 5/Program.cs
@@ -0,0 +1,50 @@
+﻿public class SelectionSort
+{
+    public static void Sort(int[] array)
+    {
+        int n = array.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++)
+            {
+                if (array[j] < array[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 5, 2, 4, 6, 1, 3 };
+
+        Console.WriteLine("Tablica przed sortowaniem:");
+        PrintArray(array);
+
+        SelectionSort.Sort(array);
+
+        Console.WriteLine("Tablica po sortowaniu:");
+        PrintArray(array);
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
