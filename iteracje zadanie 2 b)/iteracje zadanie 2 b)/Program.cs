2569cc940a97dbf6ed2d0d413a0c17ebd1867165
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-23 16:00:35 +0200

    Add files via upload

---
+++ iteracje zadanie 2 b)/iteracje zadanie 2 b)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj liczbę n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Random random = new Random();
+        int count = 0;
+
+        Console.Write("Wylosowane liczby: ");
+
+        while (count < n)
+        {
+            int number = random.Next(-4, 5) * 2;
+
+            Console.Write(number);
+
+            count++;
+            if (count < n)
+            {
+                Console.Write(", ");
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Liczba wykonanych losowań: " + count);
+    }
+}
