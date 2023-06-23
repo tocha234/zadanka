32279d5104e01f3805563b30611410bd6c59dfb8
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-23 15:58:47 +0200

    Add files via upload

---
+++ iteracje zadanie 1/iteracje zadanie 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj liczbę n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int count = 0;
+        int number = 2;
+
+        while (count < n)
+        {
+            if (IsPrime(number))
+            {
+                Console.WriteLine(number);
+                count++;
+            }
+            number++;
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
